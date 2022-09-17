a3146cf6b4f0ff1f7327e6c4a19cffd2e8f789c3
Batuhan Gürsoy <113726934+BATUGURSOY@users.noreply.github.com>
2022-09-17 12:12:42 +0300

    Create Main.java

---
+++ src/WhileDöngüsüİleÇalışmak/Main.java
@@ -0,0 +1,20 @@
+package WhileDöngüsüİleÇalışmak;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// For
+		for (int i = 2; i < 10; i += 2) {
+			System.out.println(i);
+		}
+		System.out.println("For döngüsü bitti! ");
+		int i = 2;
+		// while
+		while (i < 10) {
+			System.out.println(i);
+			i += 2;
+		}
+		System.out.println("While döngüsü bitti! ");
+	}
+
+}
