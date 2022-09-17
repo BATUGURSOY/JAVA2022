611a0f754da2a932d5eb0131abc2c4a124b7eb68
Batuhan Gürsoy <113726934+BATUGURSOY@users.noreply.github.com>
2022-09-17 12:12:18 +0300

    Create Main.java

---
+++ src/MiniProje2/Main.java
@@ -0,0 +1,20 @@
+package MiniProje2;
+
+public class Main {
+	public static void main(String[] args) {
+		char harf = 'E';
+
+		switch (harf) {
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println("Kalýn sesli harfler");
+			break;
+		default:
+			System.out.println("Ýnce sesli harfler");
+
+		}
+	}
+
+}
