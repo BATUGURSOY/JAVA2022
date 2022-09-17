03d1f47304ffa2051c0d81294b32e1a6ac7c17aa
Batuhan Gürsoy <113726934+BATUGURSOY@users.noreply.github.com>
2022-09-17 12:12:32 +0300

    Create Main.java

---
+++ src/StringlerleÇalışmak/Main.java
@@ -0,0 +1,21 @@
+package StringlerleÇalýþmak;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String mesaj = "Bugün hava çok güzel.";
+		System.out.println(mesaj);
+
+		System.out.println("Eleman sayýsý : " + mesaj.length());
+		System.out.println("5.eleman : " + mesaj.charAt(4));
+		System.out.println(mesaj.concat("Yaþasýn!"));
+		System.out.println(mesaj.startsWith("B"));
+		System.out.println(mesaj.endsWith("."));
+		char[] karakterler = new char[5];
+		mesaj.getChars(0,5,karakterler,0);
+		System.out.println(karakterler);
+		System.out.println(mesaj.indexOf("av"));
+		System.out.println(mesaj.lastIndexOf("a"));
+	}
+
+}
